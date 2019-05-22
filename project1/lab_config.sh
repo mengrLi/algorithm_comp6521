5f8ee00c58c6b644831446c65de96f4286677d09
mengrLi <34010243+mengrLi@users.noreply.github.com>
2019-05-22 16:23:06 -0400

    Add files via upload

---
+++ project1/lab_config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash                                                                                                                               
+cd build
+module load opencv
+cmake ../ -DCMAKE_CXX_COMPILER=/encs/bin/g++ -DCMAKE_CXX_FLAGS=-std=c++11
+
+cd ../
